d1d5fc7f363436c2fa570a0c36d2574f3c811883
haram O <75935548+Haram0111@users.noreply.github.com>
2024-11-21 10:30:46 +0900

    Title: 최대공약수 구하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241121/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 241121/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,7 +1,10 @@
 num = list(map(int,input().split()))
 
-answer = 1
-for i in range(1, min(num)):
-    if num[0] % i == 0 and num[1] % i == 0:
-        answer = i
-print(answer)
+def find(n,m):
+    answer = 1
+    for i in range(1, min(num)):
+        if num[0] % i == 0 and num[1] % i == 0:
+            answer = i
+    print(answer)
+
+find(num[0], num[1])
