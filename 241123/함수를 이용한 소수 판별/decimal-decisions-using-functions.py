d8604f477477796582c0acc34aa42d6b19450c65
haram O <75935548+Haram0111@users.noreply.github.com>
2024-11-23 21:31:03 +0900

    Title: 함수를 이용한 소수 판별, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241123/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,13 @@
+def prime(n):
+    if n == 1:
+        return n
+    for i in range(2,n):
+        if n % i == 0: #약수 존재
+            return 0
+    return n
+
+a, b = map(int,input().split())
+answer = 0
+for i in range(a, b+1):
+    answer += prime(i)
+print(answer)
