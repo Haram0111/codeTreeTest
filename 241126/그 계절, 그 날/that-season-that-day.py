0f215a117b686a241eecad240048945ccb528947
haram O <75935548+Haram0111@users.noreply.github.com>
2024-11-26 16:00:17 +0900

    Title: 그 계절, 그 날, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241126/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,31 @@
+def check_year(y):
+    if y % 4 == 0:
+        if y % 100 == 0:
+            if y % 400 == 0:
+                return True 
+            else:
+                return False
+        return True
+    return False
+
+def check_season(m):
+    if 3 <= m <= 5:
+        print("Spring")
+    elif 6 <= m <= 8:
+        print("Summer")
+    elif 9 <= m <= 11:
+        print("Fall")
+    else:
+        print("Winter")
+
+def check_date(y, m, d): 
+    date_lst = [31,28,31,30,31,30,31,31,30,31,30,31]
+    if check_year(y): #윤년
+        date_lst[1] = 29
+    if date_lst[m] >= d:
+        check_season(m)
+    else:
+        print("-1")
+
+y, m, d = map(int,input().split())
+check_date(y,m,d)
