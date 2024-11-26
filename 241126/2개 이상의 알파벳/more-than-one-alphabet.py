55f0c28e0bce356b951d9f574afd6291d4618a21
haram O <75935548+Haram0111@users.noreply.github.com>
2024-11-26 19:02:58 +0900

    Title: 2개 이상의 알파벳, Time: 54ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241126/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 241126/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,11 +1,11 @@
 def check_double(_str):
     lst = []
     for i in _str:
-        if len(lst) >= 2:
-            return "Yes"
         if i not in lst:
             lst.append(i)
-    return "No"
+        if len(lst) >= 2:
+            return "Yes"
+        return "No"
 
 _str = input()
 print(check_double(_str))
