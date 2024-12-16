0c62fc35d5bae04a30c1d92529e32ea32174501c
haram O <75935548+Haram0111@users.noreply.github.com>
2024-12-17 00:51:12 +0900

    Title: 오름 내림차순 정렬, Time: 71ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241217/오름 내림차순 정렬/inc-dec-sorting.py
@@ -0,0 +1,10 @@
+n = int(input())
+lst = list(map(int,input().split(" ")))
+
+lst.sort()
+for i in lst:
+    print(i, end = " ")
+print("")
+lst.sort(reverse=True)
+for i in lst:
+    print(i, end = " ")
