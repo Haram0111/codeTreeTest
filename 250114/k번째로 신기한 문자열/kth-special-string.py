a8b6bbbe01458bcc0c57e8094ee23bab4eb994db
haram Oh <75935548+Haram0111@users.noreply.github.com>
2025-01-14 16:44:35 +0900

    Title: k번째로 신기한 문자열, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250114/k번째로 신기한 문자열/kth-special-string.py
+++ 250114/k번째로 신기한 문자열/kth-special-string.py
@@ -3,5 +3,5 @@ n, k = int(n), int(k)
 str = [input() for _ in range(n)]
 
 # Write your code here!
-str.sort(key=lambda x: (not x.startswith("ap"), x))
+str.sort(key=lambda x: (not x.startswith(t), x))
 print(str[k-1])
