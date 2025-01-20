656ff53ca20f3bbec874adfd14de970a83d27b86
haram Oh <75935548+Haram0111@users.noreply.github.com>
2025-01-20 18:50:37 +0900

    Title: 연속되는 수 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250120/연속되는 수 2/continuous-number2.py
+++ 250120/연속되는 수 2/continuous-number2.py
@@ -3,13 +3,13 @@ arr = [int(input()) for _ in range(n)]
 
 # Write your code here!
 answer = 0
-pre = 0
+pre = 1
 for i in range(n):
     if i != 0:
         if arr[i-1] == arr[i]:
             pre += 1
         else:
             if pre > answer:
-                answer = pre + 1
+                answer = pre
                 pre = 0
 print(answer)
