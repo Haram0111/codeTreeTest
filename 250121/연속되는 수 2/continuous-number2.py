48aecffa8896ffc67f1287183ebdb8cb04d6add5
haram Oh <75935548+Haram0111@users.noreply.github.com>
2025-01-21 17:52:35 +0900

    Title: 연속되는 수 2, Time: 57ms, Memory: 16MB, Status: Passed - CodeTree

--- 250121/연속되는 수 2/continuous-number2.py
+++ 250121/연속되는 수 2/continuous-number2.py
@@ -1,9 +1,18 @@
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 
-# Write your code here!
-answer = 0
-for i in range(n):
-    if arr[i] != arr[i - 1] or i == 0:
-        answer += 1
+# 초기값 설정
+answer = 1  # 첫 번째 숫자부터 시작하므로 최소 1번 연속이 있을 것이다.
+pre = 1
+
+for i in range(1, n):
+    if arr[i-1] == arr[i]:  # 연속된 동일 숫자
+        pre += 1
+    else:  # 연속이 끊어짐
+        answer = max(answer, pre)  # 최대값을 갱신
+        pre = 1  # 연속 횟수 초기화
+
+# 마지막 연속 횟수 처리
+answer = max(answer, pre)
+
 print(answer)
